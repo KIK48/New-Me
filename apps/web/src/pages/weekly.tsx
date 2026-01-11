2b2b0a2c4d4efe46c9cc71c4c2ce33a2d4986090
KIK48 <kikeperezalvarez@gmail.com>
2026-01-11 00:23:13 -0700

    Added somestuff like dates on the corner added left and right buttons which will be used to go back and forward on weeks. Added the days in theinfo-displayer which needs to be tweeked so that it matches with the habbits. Added habbit strip which is a strip that will have two containers the name of the habit and the boxes. The boxes will be where the user can hover to choose if done or not which will fill the box for the specific day with an x or checkmark. I was thinking of trying click on the box for checkmark and double click for an x. If there is something in the box and want to revert the action of the box has to be done so if there is an x in the box user would have to double click to remove the x and once for the checkmark so that the box is empty.

--- apps/web/src/pages/weekly.tsx
+++ apps/web/src/pages/weekly.tsx
@@ -6,12 +6,58 @@ export default function Weekly() {
   
   return (
     <div className='Weekly focus-glow container' tabIndex={0}>
-      <div className='top-selector strip'></div>
+
+      <div className='top-selector strip'>
+        <div className='date strip strip--info letters'>1/5/2026</div>
+        <button className='arrow-btn'>
+          <svg className = "i-btn" width="18" height="18" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path 
+              d="M30 16H2M2 16L16 30M2 16L16 2" 
+              stroke="currentColor" 
+              strokeWidth="4" 
+              strokeLinecap="round" 
+            />
+          </svg>
+        </button>
+        <button className='arrow-btn'>
+          <svg className = "i-btn" width="18" height="18" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path 
+              d="M2 16H30M30 16L16 2M30 16L16 30" 
+              stroke="currentColor" 
+              stroke-width="4" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            />
+          </svg>
+
+        </button>
+        <div className='date strip strip--info letters'>1/11/2026</div>
+      </div>
+
       <div className='info-displayer strip strip--info letters'>
-        
+        Habit Mon Tues Wed Thurs Fri Sat Sun
       </div>
       <div className='habits'>
-        
+        <div className='Habit strip'>
+          <div className='Title-Habit strip letters'>Gym</div>
+          <div className='Boxes-Habit strip'>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+            <div className='box-selector'></div>
+          </div>
+        </div>
+        <div className='Habit strip'>
+          <div className='Title-Habit strip letters'>Leet</div>
+          <div className='Boxes-Habit strip'></div>
+        </div>
+        <div className='Habit strip'>
+          <div className='Title-Habit strip letters'>Clean Room</div>
+          <div className='Boxes-Habit strip'></div>
+        </div>
       </div>
     </div>
   );

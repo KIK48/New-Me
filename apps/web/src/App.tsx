edbd0ce0fdb6719244fc5677af429fa97aad43a9
KIK48 <kikeperezalvarez@gmail.com>
2026-01-18 00:35:23 -0700

    Added useContext hook. Added a file ModeContext.tsx for modes which are going to be used in the CRUD buttons that are in the weekly that are on the top. So when clicking the delete button the rings will change to  white and the minus sign will stay red. In this mode there are a new icons showing up next to the habit name. I added a tag with an x svg. This svg will only be shown in the delete mode called (D). The modes are going to help for displaying certain stuff when toggling different buttons for this issue/commit it will only affect the delete button.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/home";
 import Weekly from "./pages/weekly"
+import { ModeProvider } from "./hooks/ModeContext";
 
 
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="weekly" element={<Weekly />} />
+        <Route path="weekly" element={<ModeProvider><Weekly /></ModeProvider>} />
       </Route>
     </Routes>
   );
--- apps/web/src/components/HabitRow.tsx
+++ apps/web/src/components/HabitRow.tsx
@@ -3,6 +3,8 @@ import type { Habit, Week, HabitWeekStatus } from "../Temps/types";
 import checkIcon from "/Check square.svg";
 import xIcon from "/X square.svg";
 
+import { useMode } from "../hooks/ModeContext";
+
 type Props = {
   habit: Habit;
   week: Week;
@@ -11,9 +13,29 @@ type Props = {
 };
 
 export default function HabitRow({ habit, week, status, onToggleDay }: Props) {
+
+  const {mode} = useMode();
+
   return (
     <div className="Habit strip">
-      <div className="Title-Habit strip letters">{habit.name}</div>
+      <div className="Title-Habit strip letters">
+        <span className={mode === "D" ? "habit-name active" : "habit-name"}>{habit.name}</span> {mode === "D" && (
+          <button className="delete-svg-btn" aria-label="Toggle habit">
+            <svg className="i-delete-tag" width="39" height="35" viewBox="0 0 39 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_34_39)">
+                <path className="i-delete-jtag" d="M34.125 6H13L1.625 18L13 30H34.125C34.987 30 35.8136 29.6839 36.4231 29.1213C37.0326 28.5587 37.375 27.7956 37.375 27V9C37.375 8.20435 37.0326 7.44129 36.4231 6.87868C35.8136 6.31607 34.987 6 34.125 6Z" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path className="i-delete-jx" d="M28.75 14L19 23M19 14L28.75 23" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_34_39">
+                  <rect width="39" height="36" fill="white"/>
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
+
+        )}
+      </div> {/* Here we check for mode for delete rn */}
 
       <div className="Boxes-Habit strip">
         {week.days.map((dayIso) => {

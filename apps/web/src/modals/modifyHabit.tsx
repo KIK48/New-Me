0c1fe3c90b5e319fb5a47d91e8783d6d887bec4d
KIK48 <kikeperezalvarez@gmail.com>
2026-01-18 22:37:44 -0700

    I added a modal where the user will be able to modify the habit. I made it class container with no focus though. Added a few things to the modal like save and cancel might make the modal reusable because the save function i want a modal too and would be nice to keep it uniform just change what the modal actually displays.

---
+++ apps/web/src/modals/modifyHabit.tsx
@@ -0,0 +1,35 @@
+import { createPortal } from "react-dom";
+import type { Habit } from "../Temps/types";
+import "../styles/modals/modifyHabit.css"
+
+type Props = {
+  h: Habit;
+  onClose: () => void;
+};
+
+export default function HabbitMod({ h, onClose }: Props) {
+  const modalRoot = document.getElementById("root");
+  if (!modalRoot) return null;
+
+  return createPortal(
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal container letters" onClick={(e) => e.stopPropagation()}>
+        <h2 className="modal-title">Edit Habit</h2>
+        <div className="modal-content">
+          <div className="habit-content">
+            <p className="modal-text letters">{h.name}:</p>
+            <input type="text" className="input-hname letters"></input>
+          </div>
+        </div>
+
+        <div className="modal-actions">
+          <button className="btn-secondary letters" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="btn-primary letters" onClick={onClose}>Save</button>
+        </div>
+      </div>
+    </div>,
+    modalRoot
+  );
+}

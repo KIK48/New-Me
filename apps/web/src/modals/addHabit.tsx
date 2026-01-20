9f3f8f0a3a4395b39183414f921093c5ae7d36e7
kik48 <kikeperezalvarez@gmail.com>
2026-01-20 10:31:14 -0700

    Did the same thing as the modify modal just changed a few things but they are basically almost the same thing. Also in line 101 there is like an error that I need to investigate because I'm not sure if it's bad or it's just complaining because it is null.

---
+++ apps/web/src/modals/addHabit.tsx
@@ -0,0 +1,34 @@
+import { createPortal } from "react-dom";
+import type { Habit } from "../Temps/types";
+import "../styles/modals/modifyHabit.css"
+
+type Props = {
+  onClose: () => void;
+};
+
+export default function AddHabit({ onClose }: Props) {
+  const modalRoot = document.getElementById("root");
+  if (!modalRoot) return null;
+
+  return createPortal(
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal container letters" onClick={(e) => e.stopPropagation()}>
+        <h2 className="modal-title">Adding New Habit</h2>
+        <div className="modal-content">
+          <div className="habit-content">
+            <p className="modal-text letters">Habit Name: </p>
+            <input type="text" className="input-hname letters"></input>
+          </div>
+        </div>
+
+        <div className="modal-actions">
+          <button className="btn-secondary letters" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="btn-primary letters" onClick={onClose}>Save</button>
+        </div>
+      </div>
+    </div>,
+    modalRoot
+  );
+}
